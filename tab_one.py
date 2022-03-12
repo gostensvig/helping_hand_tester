5b215d19345447d2fcdde634c327a16bf65aaf46
Gjermund Oestensvig <gostensvig@vicarious.com>
2022-03-11 20:09:26 -0800

    Removed write function in tab_one

--- tab_one.py
+++ tab_one.py
@@ -10,21 +10,28 @@ class Tab1(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
-        add_label = Label(self, text="Log-Timestamp to Send:")
-        add_label.pack(fill="both")
+        self.timestamp_list = []
 
-        entry = Entry(self, width=25)
-        entry.insert(0, "20xx-xx-xx-xx-xx-xx-xxx")
-        entry.pack()
+        self.add_label = Label(self, text="Log-Timestamp to Send:")
+        self.add_label.pack(fill="both")
 
-        button = Button(
-            self, text="Add", command=lambda: write_timestamp_to_file(entry.get())
+        self.entry = Entry(self, width=25)
+        self.entry.insert(0, "20xx-xx-xx-xx-xx-xx-xxx")
+        self.entry.pack()
+
+        self.button = Button(
+            self,
+            text="Add",
+            command=lambda: write_timestamp_to_file(self, self.entry.get()),
         )
-        button.pack()
+        self.button.pack()
 
-        queue_label = Label(self, text="Queued Timestamps:")
-        queue_label.pack(fill="both")
+        self.queue_label = Label(self, text="Queued Timestamps:")
+        self.queue_label.pack(fill="both")
 
-        list_label = Label(self, text="Dis here")
-        list_label.pack(fill="both")
+        self.list_label = Label(self, text="Dis here")
+        self.list_label.pack(fill="both")
         # TODO Create a way to update the list_label to match the contents of timestamp_queue.txt
+
+    def update_list():
+        print("Updating list_label")
